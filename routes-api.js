0a24eb65e838098474bbfb09fe800f38956e6c80
sarahbarron <sarahflavin@yahoo.com>
2020-05-23 00:05:31 +0100

    user api and tests added

--- routes-api.js
+++ routes-api.js
@@ -1,16 +1,24 @@
 const Categories = require('./app/api/categories');
 const Pois = require('./app/api/poi');
+const Users = require('./app/api/user');
 
 module.exports=[
-  { method: 'GET', path: '/api/pois', config: Pois.find },
-  { method: 'GET', path: '/api/pois/{id}', config: Pois.findOne },
-  { method: 'POST', path: '/api/pois', config: Pois.create },
-  { method: 'DELETE', path: '/api/pois/{id}', config: Pois.deleteOne },
-  { method: 'DELETE', path: '/api/pois', config: Pois.deleteAll },
+  { method: 'GET', path: '/api/users', config: Users.find },
+  { method: 'GET', path: '/api/users/{id}', config: Users.findOne },
+  { method: 'POST', path: '/api/users', config: Users.create },
+  { method: 'DELETE', path: '/api/users/{id}', config: Users.deleteOne },
+  { method: 'DELETE', path: '/api/users', config: Users.deleteAll },
+
+  // { method: 'GET', path: '/api/pois', config: Pois.find },
+  // { method: 'GET', path: '/api/pois/{id}', config: Pois.findOne },
+  // { method: 'POST', path: '/api/pois', config: Pois.create },
+  // { method: 'DELETE', path: '/api/pois/{id}', config: Pois.deleteOne },
+  // { method: 'DELETE', path: '/api/pois', config: Pois.deleteAll },
 
   { method: 'GET', path: '/api/categories', config: Categories.find },
   { method: 'GET', path: '/api/categories/{id}', config: Categories.findOne },
   { method: 'POST', path: '/api/categories', config: Categories.create },
   { method: 'DELETE', path: '/api/categories/{id}', config: Categories.deleteOne },
   { method: 'DELETE', path: '/api/categories', config: Categories.deleteAll },
+
 ];
--- test/poi-service.js
+++ test/poi-service.js
@@ -1,11 +1,55 @@
 'use strict';
 
 const axios = require('axios');
-const baseUrl = 'http://localhost:3000';
 
 class PoiService {
   constructor(baseUrl) {
     this.baseUrl = baseUrl;
+  }
+
+  async getUsers() {
+    try {
+      const response = await axios.get(this.baseUrl + '/api/users');
+      return response.data;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async getUser(id) {
+    try {
+      const response = await axios.get(this.baseUrl + '/api/users/' + id);
+      return response.data;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async createUser(newUser) {
+    try {
+      const response = await axios.post(this.baseUrl + '/api/users', newUser);
+      return response.data;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async deleteAllUsers() {
+    try {
+      const response = await axios.delete(this.baseUrl + '/api/users');
+      return response.data;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async deleteOneUser(id) {
+    try {
+      const response = await axios.delete(this.baseUrl + '/api/users/' + id);
+      return response.data;
+    } catch (e) {
+      return null;
+    }
   }
 
   async getCategories() {
@@ -37,25 +81,7 @@ class PoiService {
     return response.data;
   }
 
-  // async getUsers() {
-  //   const response = await axios.get(this.baseUrl + '/api/users');
-  //   return response.data;
-  // }
-  //
-  // async getUser(id) {
-  //   try {
-  //     const response = await axios.get(this.baseUrl + '/api/user/' + id);
-  //     return response.data;
-  //     return response.data;
-  //   } catch (e) {
-  //     return null;
-  //   }
-  // }
-  //
-  // async createUser(newUser) {
-  //   const response = await axios.post(this.baseUrl + '/api/users', newUser);
-  //   return response.data;
-  // }
+
 }
 
 module.exports = PoiService;
